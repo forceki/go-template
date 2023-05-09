83356a5d7939cfc1d2e8c6e3c15f12dec6d91fbd
forceki <forcekidesu@gmail.com>
2023-05-09 13:15:51 +0700

    time

--- router/router.go
+++ router/router.go
@@ -2,11 +2,6 @@ package router
 
 import (
 	"github.com/forceki/invent-be/src/auth"
-	"github.com/forceki/invent-be/src/checkin"
-	"github.com/forceki/invent-be/src/gudang"
-	"github.com/forceki/invent-be/src/items"
-	"github.com/forceki/invent-be/src/kategori"
-	"github.com/forceki/invent-be/src/supplier"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
@@ -14,11 +9,6 @@ import (
 func SetupRoutes(db *gorm.DB, app *fiber.App) {
 	api := app.Group("/api")
 
-	items.ItemsRouter(db, api)
-	gudang.GudangRouter(db, api)
-	supplier.SupplierRouter(db, api)
-	checkin.CheckinRouter(db, api)
-	kategori.KategoriRouter(db, api)
 	auth.AuthRouter(db, api)
 
 	api.Get("/test", testin)
